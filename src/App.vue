0fbd41435c582cc86e804e689153e36824be7214
readyshen <readyshen@gmail.com>
2025-02-11 02:10:59 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    name:<input v-model="name" />{{ name }}<br />
+    salary:<input type="number" v-model="salary" />{{ salary }}<br />
+    <button v-on:click="addSalary">提交</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: 'roy',
+      salary: 15000,
+    }
+  },
+  methods: {
+    addSalary() {
+      this.salary = Number(this.salary) + 1000
+    },
+  },
+}
+</script>
+
+<style scoped></style>
