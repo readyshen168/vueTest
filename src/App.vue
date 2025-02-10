73d29f841b7a69a6b67af3a025b04c5216016822
readyshen <readyshen@gmail.com>
2025-02-11 03:07:26 +0800

    设定双向绑定

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,26 @@
     name:<input v-model="name" />{{ name }}<br />
     salary:<input type="number" v-model="salary" />{{ salary }}<br />
     <button v-on:click="addSalary">提交</button>
+    <hr />
+
+    <div>
+      <h2>个人信息</h2>
+      <p>age:<input type="number" v-model="userinfo.age" />{{ userinfo.age }}</p>
+      <p>
+        gender: <input type="radio" name="gender" v-model="userinfo.gender" />男
+        <input type="radio" name="gender" v-model="userinfo.gender" />女
+      </p>
+      <p>
+        岗位：
+        <select name="jobs" v-model="userinfo.job">
+          <option value="frontend">前端</option>
+          <option value="backend">后端</option>
+          <option value="devops">运维</option>
+          <option value="testing">测试</option>
+        </select>
+      </p>
+      <p>个人信息汇总：{{ userinfo }}</p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +32,11 @@ export default {
     return {
       name: 'roy',
       salary: 15000,
+      userinfo: {
+        age: 18,
+        gender: 'male',
+        job: 'devops',
+      },
     }
   },
   methods: {
